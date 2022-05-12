4a3299c5af587c4e098cccc7fb139445938f40a0
Misaki <64830812+misaki-web@users.noreply.github.com>
2022-05-12 20:58:43 +0200

    Updating jQuery and Bootstrap

--- IniEditor.class.php
+++ IniEditor.class.php
@@ -160,13 +160,13 @@ class IniEditor
 	public static function getCssJsInclude()
 	{
 		return <<<'HEREDOC'
-			<script src="https://code.jquery.com/jquery-3.1.1.min.js"
-			        integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+			<script src="https://code.jquery.com/jquery-3.6.0.min.js"
+			        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
 			        crossorigin="anonymous"></script>
 			<link rel="stylesheet"
-			      type="text/css"
-			      media="screen"
-			      href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+			      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+			      integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+			      crossorigin="anonymous" />
 			HEREDOC;
 	}
 	
